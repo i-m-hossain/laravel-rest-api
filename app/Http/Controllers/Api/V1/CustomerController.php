35272000f374c0461ba3bd8f44591f7defc1cd0f
mdimran1409036 <mdimran1409036@gmail.com>
2023-05-14 00:42:07 +0600

    query format added

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,16 +8,24 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
+use App\Services\V1\CustomerQuery;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $filter = new CustomerQuery();
+        $queryItems = $filter-> transform($request);
+        if(count($queryItems) == 0){
+            return new CustomerCollection(Customer::paginate(20));
+        }else{
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
 
-        return new CustomerCollection(Customer::paginate(20));
 
     }
 
